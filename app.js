d99f62dfcb99f4474188b669fc4831d9d0ff971f
Zidane Innis <zidaneinnis@gmail.com>
2023-04-18 14:51:59 -0500

    Styling our pages

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -8,11 +10,13 @@ const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded());
 
-app.use("/admin", adminRoutes);
+app.use(adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res
+    .status(404)
+    .sendFile(path.join(__dirname, "views", "page-not-found.html"));
 });
 
 app.listen(3000);
